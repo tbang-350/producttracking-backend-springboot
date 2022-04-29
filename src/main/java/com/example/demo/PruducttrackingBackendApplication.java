71960ee949ef769281e47b59f418161bfa4a196a
tbang-350 <tbway350@gmail.com>
2022-04-29 15:49:11 +0300

    first commit

---
+++ src/main/java/com/example/demo/PruducttrackingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruducttrackingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruducttrackingBackendApplication.class, args);
+	}
+
+}
